2b5c329c7500e485a47ed38c22e539b6b3eeb1db
gb3h <gabriel.yfy@gmail.com>
2019-06-26 12:50:32 +0800

    Added (not yet functional) calculate button that is generated when UpdateButtons is called. Proper functionality similar to Place Buttons also added to it.

--- index.js
+++ index.js
@@ -290,10 +290,19 @@ function UpdateButtons(listOfLocations, map) {
   } else {
     document.createElement('NavButtons');
   }
+  if (document.getElementById('CalculateButton')) {
+    document.getElementById('CalculateButton').childNodes.forEach(function(child){
+      document.getElementById('CalculateButton').removeChild(child);
+    })
+  } else {
+    document.createElement('CalculateButton');
+  }
   var newButtons = document.createElement('NavButtons');
+  var calculateButtons = document.createElement('CalculateButton');
 
   // Clearing the control MVC array just in case also.
   map.controls[google.maps.ControlPosition.RIGHT_CENTER].clear();
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].clear();
 
   // For each place in the list of locations, we add a button for it on the RIGHT_CENTER of the map.
   for (var i = 0; i < listOfLocations.length; i++) {
@@ -302,4 +311,39 @@ function UpdateButtons(listOfLocations, map) {
     var placeButton = new PlaceButton(placeButtonDiv, map, listOfLocations[i], listOfLocations);
     map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(placeButtonDiv);
   }
+
+  var calculateButtonDiv = document.createElement('div');
+  calculateButtons.appendChild(calculateButtonDiv);
+  var calculateButton = new CalculateButton(calculateButtonDiv, map, listOfLocations);
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(calculateButtonDiv);
+}
+
+function CalculateButton(controlDiv, map, listOfLocations) {
+  // Set CSS for the button background.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = '#fff';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '3px';
+  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.marginTop = '-3px';
+  controlUI.style.marginBottom = '1px';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to view place';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.color = 'rgb(25,25,25)';
+  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlText.style.fontSize = '16px';
+  controlText.style.lineHeight = '38px';
+  controlText.style.paddingLeft = '10px';
+  controlText.style.paddingRight = '8px';
+  controlText.innerHTML = 'Calculate!';
+  controlUI.appendChild(controlText);
+
+  // Setup the click event listeners: simply set the map to the chosen location.
+  controlUI.addEventListener('click', function() {
+  });
 }
