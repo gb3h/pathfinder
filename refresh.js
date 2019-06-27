ac43996965267b8303d6d64f6fa8b0343675efab
febee99 <febee99@gmail.com>
2019-06-28 00:48:51 +0800

    Refresh page function for new path button

---
+++ refresh.js
@@ -0,0 +1,5 @@
+function refresh() {
+  if (confirm("Clicking OK will delete any path made. \nClick cancel and save this path if you have not before proceeding" )) {
+    window.location.reload();
+  } else {}
+}
