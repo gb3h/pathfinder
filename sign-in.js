9e27aef12e28b379397dc1f76e7eb2e5a0d3f300
febee99 <febee99@gmail.com>
2019-06-28 22:40:00 +0800

    Added welcome notification upon sign-in

--- sign-in.js
+++ sign-in.js
@@ -1,12 +1,3 @@
-function onSuccess(googleUser) {
-  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-  window.location.reload();
-}
-
-function onFailure(error) {
-  console.log(error);
-}
-
 function renderButton() {
   gapi.signin2.render('g-signin2', {
     'scope': 'profile email',
@@ -17,4 +8,36 @@ function renderButton() {
     'onsuccess': onSuccess,
     'onfailure': onFailure
   });
+}
+
+function onSuccess(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+  //pass information to server to insert or update the user record
+  window.location.href = '#';
+  setTimeout(function() {welcome(profile);}, 1000);
+}
+
+function onFailure(error) {
+  console.log(error);
+}
+
+function welcome(profile){
+  console.log("Welcome notification");
+  GrowlNotification.notify({
+              title: "Welcome " + profile.getName().split(" ")[0],
+              description: 'You have 3 saved paths with us',
+              image: {
+                visible: true,
+                customImage: profile.getImageUrl()
+              },
+              type: 'alert',
+              position: 'top-right',
+              closeTimeout: 3000,
+              animationDuration: 5,
+
+            }); // or (new GrowlNotification(options)).show();
 }
