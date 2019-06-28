e618a3f9c590adbc152d66236691431a9e3fb534
febee99 <febee99@gmail.com>
2019-06-28 22:38:54 +0800

    Added dummy routes that load after being selected in pop-up

---
+++ dummy-routes.js
@@ -0,0 +1,264 @@
+function route1() {
+  console.log("Loading route 1.....");
+  window.location.href = '#';
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    backgroundColor: "#0099dd",
+    center: {lat: 1.352083, lng: 103.819839}, //Singapore-centered
+    zoom: 3,
+    maxZoom: 15,
+    minZoom: 3,
+    mapTypeId: 'roadmap',
+    disableDefaultUI: true,
+    gestureHandling: 'greedy',
+    mapTypeControlOptions: {
+      mapTypeIds: []
+    },
+    styles:[
+      {
+          "stylers": [
+              {
+                  "saturation": -100
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#0099dd"
+              }
+          ]
+      },
+      {
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#aadd55"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {}
+  ]
+  });
+  var locations = [{name: "Westgate", place_id: "ChIJ82AoVA8Q2jERkHnr1RPW9B4"},
+  {name: "Jurong East", place_id: "ChIJL52pCg8Q2jERgiWsODe3X9I"}];
+  directionsDisplay.setMap(map);
+  UpdateButtons(locations, map, directionsService, directionsDisplay);
+  calculateAndDisplayRoute(directionsService, directionsDisplay,locations);
+};
+
+function route2() {
+  console.log("Loading route 2.....");
+  window.location.href = '#';
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    backgroundColor: "#0099dd",
+    center: {lat: 1.352083, lng: 103.819839}, //Singapore-centered
+    zoom: 3,
+    maxZoom: 15,
+    minZoom: 3,
+    mapTypeId: 'roadmap',
+    disableDefaultUI: true,
+    gestureHandling: 'greedy',
+    mapTypeControlOptions: {
+      mapTypeIds: []
+    },
+    styles:[
+      {
+          "stylers": [
+              {
+                  "saturation": -100
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#0099dd"
+              }
+          ]
+      },
+      {
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#aadd55"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {}
+  ]
+  });
+  directionsDisplay.setMap(map);
+  var locations = [{name: "Westgate", place_id: "ChIJ82AoVA8Q2jERkHnr1RPW9B4"},
+  {name: "Jurong East", place_id: "ChIJL52pCg8Q2jERgiWsODe3X9I"}];
+  directionsDisplay.setMap(map);
+  UpdateButtons(locations, map, directionsService, directionsDisplay);
+  calculateAndDisplayRoute(directionsService, directionsDisplay,locations);
+};
+
+function route3() {
+  console.log("Loading route 3.....");
+  window.location.href = '#';
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    backgroundColor: "#0099dd",
+    center: {lat: 1.352083, lng: 103.819839}, //Singapore-centered
+    zoom: 3,
+    maxZoom: 15,
+    minZoom: 3,
+    mapTypeId: 'roadmap',
+    disableDefaultUI: true,
+    gestureHandling: 'greedy',
+    mapTypeControlOptions: {
+      mapTypeIds: []
+    },
+    styles:[
+      {
+          "stylers": [
+              {
+                  "saturation": -100
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#0099dd"
+              }
+          ]
+      },
+      {
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#aadd55"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {}
+  ]
+  });
+  var locations = [{name: "Westgate", place_id: "ChIJ82AoVA8Q2jERkHnr1RPW9B4"},
+  {name: "Jurong East", place_id: "ChIJL52pCg8Q2jERgiWsODe3X9I"}];
+  directionsDisplay.setMap(map);
+  UpdateButtons(locations, map, directionsService, directionsDisplay);
+  calculateAndDisplayRoute(directionsService, directionsDisplay,locations);
+};
