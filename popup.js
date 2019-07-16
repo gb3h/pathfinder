22752773c012959f55a5085fbca19ec76e0de3ca
febee99 <febee99@gmail.com>
2019-07-16 21:24:24 +0800

    Added delete path functionality

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ function checkIfLoggedIn(){
   if(gapi.auth2.getAuthInstance().isSignedIn.get()){
     alert("Signed in");
     document.getElementById("view").setAttribute("href", "#popup2");
-    insertRoute();
+    getLocations();
   } else {
     alert("Signed out");
     document.getElementById("view").setAttribute("href", "#popup1");
@@ -10,7 +10,7 @@ function checkIfLoggedIn(){
   return false;
 }
 
-function insertRoute(){
+function getLocations(){
   var req = new XMLHttpRequest();
   var url = '/paths';
   req.open('GET',url,true);
@@ -24,30 +24,73 @@ function onLoad() {
    console.log(response);
    var all_routes = JSON.parse(response);
    console.log(all_routes);
-
-    //Adding li nodes dynamically according to the number of saved routes
-    var ul = document.getElementById("myList");
-
-   if (!ul.hasChildNodes()) {
-     all_routes.forEach(function(routex, i) {
-
-       var node = document.createElement("li");                     // Create a <li> node
-       var textnode = document.createTextNode("Route " + (i + 1));  // Create a text node
-       node.appendChild(textnode);                                  // Append the text to <li>
-       ul.appendChild(node);     // Append <li> to <ul> with id="myList"
-       node.onclick = function() {
-          initAutocomplete.callUpdateButtons(routex.route);
-          initAutocomplete.callCalculateAndDisplay(routex.route, false);
-          window.location.href = '#';
-       };
-     });
-    } else {
-       console.log("Already contain");
-    }
-
+   createList(all_routes);
 }
 
 function onError() {
   // handle error here, print message perhaps
   console.log('error receiving async AJAX call');
+}
+
+function createList(all_routes){
+  //Adding li nodes dynamically according to the number of saved routes
+  var ul = document.getElementById("myList");
+
+ if (!ul.hasChildNodes()) {
+   all_routes.forEach(function(routex, i) {
+
+     var node = document.createElement("li");                     // Create a <li> node
+     node.setAttribute("id", routex._id);
+     routex.route.forEach(function(location, i) {
+       if (i >= 1){
+         node.appendChild(document.createTextNode(", " + location.name));
+       } else {
+         node.appendChild(document.createTextNode(location.name));
+       }
+     });
+
+     var details = document.createElement("more-details");
+     // var options = { weekday: 'long',
+     //                 year: 'numeric',
+     //                 month: 'long',
+     //                 day: 'numeric' ,
+     //                 hour: 'numeric',
+     //                 minute: 'numeric'};
+
+     // details.appendChild(document.createTextNode(new Date(routex.created_at).toLocaleString("en-US", options)));
+     details.appendChild(document.createTextNode(moment(routex.created_at).fromNow()));
+     node.append(details);
+
+     createDeleteButton(node);
+     node.onclick = function() {
+        initAutocomplete.callUpdateButtons(routex.route);
+        initAutocomplete.callCalculateAndDisplay(routex.route, false);
+        window.location.href = '#';
+     };
+     ul.appendChild(node);
+        // Append <li> to <ul> with id="myList"
+   });
+  } else {
+     console.log("Already contain");
+  }
+}
+
+function createDeleteButton(node){
+  var removeButton = document.createElement('button');
+  removeButton.appendChild(document.createTextNode("delete"));
+  node.appendChild(removeButton);
+  removeButton.onclick = function(e) {
+    deletePath(node);
+    e.stopPropagation();
+    node.removeChild(removeButton);
+    node.remove();
+  }
+}
+
+function deletePath(node){
+  $.ajax({
+     type: "POST",
+     data: {_id: node.id},
+     url: "delete-path"
+  });
 }
