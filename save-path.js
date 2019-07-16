b0dee4988e92ed4f9a7779d9ea7fdd911b852e38
febee99 <febee99@gmail.com>
2019-07-16 14:08:20 +0800

    Clean path, post locations to database, notify saved functions

--- save-path.js
+++ save-path.js
@@ -1,15 +1,54 @@
-//Stored variable
 var locations = [];
 
-function savePath(directionsService, directionsDisplay, listOfLocations) {
-  for (var i = 0; i < listOfLocations; i++) {
-    locations.push(listOfLocations[i]);
-    console.log(locations[i].place_id);
-  }
+function cleanPath(listOfLocations){
+  locations = []; //refresh everytime the path is updated
+  listOfLocations.forEach(function(location){
+      locations.push({
+        name: location.name,
+        place_id: location.place_id
+      });
+  });
 }
 
 function savePath(){
-  for (var i = 1; i < locations.length; i++) {
-    console.log(locations[i].place_id);
+  if(gapi.auth2.getAuthInstance().isSignedIn.get()){
+    $.ajax({
+       type: "POST",
+       data: {locations: locations},
+       url: "save-path",
+    }).done(function(data){
+         console.log(data);
+         window.location.href = '#';
+    });
+    notifySave();
+  } else {
+      document.getElementById("save").setAttribute("href", "#popup1");
   }
+}
+
+function notifySave(){
+  var title = "Successful save";
+  var description = JSON.stringify(locations, ['name']);
+  var type = "success";
+  console.log("Saving in progress...");
+  if (locations == undefined || locations.length <= 0){
+      title = "Save failed, no locations entered";
+      description = "Please try again"
+      type = "error";
+      console.log(locations);
+  } else {}
+  GrowlNotification.notify({
+    title: title,
+    description: description,
+    image: {
+      visible: true,
+      customImage: ''
+    },
+    closeTimeout: 3000,
+    type: type,
+    closeWith: ['click', 'button'],
+    animationDuration: .5,
+    position: 'top-center'
+
+});
 }
