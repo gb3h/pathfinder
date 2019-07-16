abf8a16116dc75473760cbab373784c51bd98001
febee99 <febee99@gmail.com>
2019-07-16 14:07:24 +0800

    Post user data to database

--- sign-in.js
+++ sign-in.js
@@ -10,34 +10,53 @@ function renderButton() {
   });
 }
 
-function onSuccess(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId());
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
-  //pass information to server to insert or update the user record
-  window.location.href = '#';
-  setTimeout(function() {welcome(profile);}, 1000);
-}
-
 function onFailure(error) {
   console.log(error);
 }
 
-function welcome(profile){
-  console.log("Welcome notification");
-  GrowlNotification.notify({
-              title: "Welcome " + profile.getName().split(" ")[0],
-              description: 'You have 3 saved paths with us',
-              image: {
-                visible: true,
-                customImage: profile.getImageUrl()
-              },
-              type: 'alert',
-              position: 'top-right',
-              closeTimeout: 3000,
-              animationDuration: 5,
+function onSuccess(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  // console.log("Successful login. Loading user info...")
+  // console.log('ID: ' + profile.getId());
+  // console.log('Name: ' + profile.getName());
+  // console.log('Image URL: ' + profile.getImageUrl());
+  // console.log('Email: ' + profile.getEmail());
+  //pass information to server to insert or update the user record
+  window.location.href = '#';
+  setTimeout(function() {welcome(profile);}, 1000);
+  updateUserData(profile);
+}
 
-            }); // or (new GrowlNotification(options)).show();
+function welcome(profile){
+  console.log("Welcome notification loading...");
+  GrowlNotification.notify({
+    title: "Welcome " + profile.getName().split(" ")[0],
+    description: '',
+    image: {
+      visible: true,
+      customImage: ''
+      //profile.getImageUrl()
+    },
+    closeTimeout: 3000,
+    type: 'default',
+    closeWith: ['click', 'button'],
+    animationDuration: .5,
+    position: 'top-center'
+
+});
+console.log("Welcome complete!!");
+// or (new GrowlNotification(options)).show();
+}
+
+function updateUserData(profile){
+  if(profile){
+      $.ajax({
+            type: 'POST',
+            url: 'save-user',
+            data: {id:profile.getId(), name:profile.getName(), email:profile.getEmail()}
+        }).done(function(data){
+            console.log(data);
+            window.location.href = '#';
+        })
+  } 
 }
