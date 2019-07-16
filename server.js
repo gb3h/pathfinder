ee4990e3b419225f4b51501cd2feea1d62377b98
febee99 <febee99@gmail.com>
2019-07-16 21:24:43 +0800

    Added delete path functionality

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ var userSchema = new mongoose.Schema({
 // Create Route Schema for stored route data
 var routeSchema = new mongoose.Schema({
   user_email: {type: String, required: true},
-  route: {type : [locationSchema], required: true}
+  route: {type : [locationSchema], required: true},
+  created_at: { type: Date, required: true, default: Date.now }
 });
 
 // Create collections named users and routes
@@ -75,6 +76,7 @@ app.post('/save-path', function(req, res){
       console.log(err);
     } else if (routes_found.length == 0){
       console.log("Saving first ever route...");
+      console.log(route);
       route.save();
     } else if (routes_found.length > 0){
       console.log(routes_found.length);
@@ -109,6 +111,21 @@ app.post('/save-path', function(req, res){
   res.redirect('/');
 });
 
+app.post('/delete-path', function(req, res) {
+  if (user.email != undefined) {
+    console.log(req.body._id);
+    Route.deleteOne({_id: req.body._id}, function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Deleted route with id " + req.body._id);
+      }
+    });
+  } else {
+    // console.log("No email");
+    // res.redirect("/");
+  }
+});
 app.get('/paths', function(req, res) {
   if (user.email != undefined) {
     Route.find({user_email: user.email}, function(err, all_routes){
