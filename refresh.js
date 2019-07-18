3247e1c6a29991732a21bcf97cda035957c575d1
febee99 <febee99@gmail.com>
2019-07-19 01:45:38 +0800

    Formatted file

--- refresh.js
+++ refresh.js
@@ -1,30 +1,33 @@
+/*global document, window, alert, console, require, google, gapi, moment, XMLHttpRequest*/
+
+var GrowlNotification;
+
 function refresh() {
-  // if (confirm("Clicking OK will delete any path made. \nClick cancel and save this path if you have not before proceeding" )) {
-  //   window.location.reload();
-  // } else {}
-
-  GrowlNotification.notify({
-              title: "Warning",
-              description: 'This will delete any unsaved path made.',
-              type: 'error',
-              position: 'top-center',
-              closeTimeout: 3000,
-              animationDuration: 5,
-              image: {
-                  visible: true,
-                  customImage: ''
-              },
-              showButtons: true,
-              buttons: {
-                  cancel: {
-                      text: 'Cancel',
-                      callback: function() {}
-                  },
-                  action: {
-                      text: 'Ok',
-                      callback: function() {window.location.reload();}
-                  },
-              },
-
-            }); // or (new GrowlNotification(options)).show();
+    // if (confirm("Clicking OK will delete any path made. \nClick cancel and save this path if you have not before proceeding" )) {
+    //   window.location.reload();
+    // } else {}
+    "use strict";
+    GrowlNotification.notify({
+        title: "Warning",
+        description: 'This will delete any unsaved path made.',
+        type: 'error',
+        position: 'top-center',
+        loseTimeout: 3000,
+        animationDuration: 5,
+        image: {
+            visible: true,
+            customImage: ''
+        },
+        showButtons: true,
+        buttons: {
+            cancel: {
+                text: 'Cancel',
+                callback: function () { return; }
+            },
+            action: {
+                text: 'Ok',
+                callback: function () {window.location.reload(); }
+            }
+        }
+    }); // or (new GrowlNotification(options)).show();
 }
