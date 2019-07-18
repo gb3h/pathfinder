54cd2a0ea67716073b4912564df2fe2d8189a13d
febee99 <febee99@gmail.com>
2019-07-19 01:47:51 +0800

    Formatted file, added comments

--- vertices-panel2.js
+++ vertices-panel2.js
@@ -1,43 +1,51 @@
-function VerticesPanel(resultObject, map, listOfLocations) {
+/*global document, window, alert, console, confirm, google, gapi, moment, $, setTimeout*/
+
+var Sortable, cleanPath, initAutocomplete;
+
+function verticesPanel(map, listOfLocations) {
+    "use strict";
     // Clearing out old buttons just in case (hard update)
     if (document.getElementById('VerticesPanel')) {
-      document.getElementById('VerticesPanel').childNodes.forEach(function(child){
-        document.getElementById('VerticesPanel').removeChild(child);
-      })
+        document.getElementById('VerticesPanel').childNodes.forEach(function (child) {
+            document.getElementById('VerticesPanel').removeChild(child);
+        });
     } else {
-      document.createElement('VerticesPanel');
+        document.createElement('VerticesPanel');
     }
-
     map.controls[google.maps.ControlPosition.TOP_CENTER].clear();
-    var vp = document.createElement('ul');
+
+    // Create vertices panel.
+    var vp = document.createElement('ul'),
+        sortable = new Sortable(vp, {
+            animation: 150,
+            filter: '.filtered',
+            onUpdate: function () {
+                var i, newOrderedLocations = [],
+                    listElements = $("#sortable").children();
+                console.log(listElements.length);
+                for (i = 0; i < listElements.length; i += 1) {
+                    console.log(listElements[i].innerText + " " + listElements[i].getAttribute('place_id'));
+                    newOrderedLocations.push({
+                        name: listElements[i].innerText,
+                        place_id: listElements[i].getAttribute('place_id')
+                    });
+                }
+                cleanPath(newOrderedLocations);
+                initAutocomplete.callCalculateAndDisplay(newOrderedLocations, false);
+            }
+        });
+
     vp.setAttribute('id', 'sortable');
-    listOfLocations.forEach(function(location){
-      var li = document.createElement("li");                     // Create a <li> node
-      li.setAttribute("class", "ui-state-default");
-      li.innerHTML = location.name;
-      li.setAttribute("place_id", location.place_id);
+    listOfLocations.forEach(function (location) {
+        var li = document.createElement("li"),                  // Create a <li> node
+            arrow = document.createElement("span");
+        li.setAttribute("class", "ui-state-default");
+        li.innerHTML = location.name;
+        li.setAttribute("place_id", location.place_id);
 
-      var arrow = document.createElement("span");
-      arrow.setAttribute("class", "ui-icon ui-icon-arrowthick-2-n-s");
-      li.appendChild(arrow);
-      vp.appendChild(li);
-
-    });
-    var sortable = new Sortable(vp, {animation: 150, filter: '.filtered', onUpdate: function (/**Event*/evt) {
-        var newOrderedLocations = [];
-        var listElements = $("#sortable").children();
-        console.log(listElements.length);
-        for (var i = 0; i < listElements.length; i++){
-          console.log(listElements[i].innerText + " " + listElements[i].getAttribute('place_id'));
-          newOrderedLocations.push({
-            name: listElements[i].innerText,
-            place_id: listElements[i].getAttribute('place_id')
-          });
-        }
-        cleanPath(newOrderedLocations);
-        initAutocomplete.callCalculateAndDisplay(newOrderedLocations, false);
-
-      }
+        arrow.setAttribute("class", "ui-icon ui-icon-arrowthick-2-n-s");
+        li.appendChild(arrow);
+        vp.appendChild(li);
     });
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(vp);
-  }
+}
