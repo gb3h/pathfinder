76004d3f4e404afbc79fb24b4554292197c53fd6
febee99 <febee99@gmail.com>
2019-07-19 01:47:13 +0800

    Formatted file, added comments

--- sign-out.js
+++ sign-out.js
@@ -1,8 +1,14 @@
+/*global document, window, alert, console, confirm, google, gapi, moment, $, setTimeout*/
+
+// Signs user out
 function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  if (confirm("Clicking this button will sign you out from Google!\n Click OK to proceed." )) {
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  } else {}
+    "use strict";
+    var auth2 = gapi.auth2.getAuthInstance();
+
+    // Alert confirmation for sign-out
+    if (confirm("Clicking this button will sign you out from Google!\n Click OK to proceed.")) {
+        auth2.signOut().then(function () {
+            console.log('User signed out.');
+        });
+    }
 }
