e17232e47e3b83ca0a510fe43f99a69eef7598d1
febee99 <febee99@gmail.com>
2019-07-19 01:46:54 +0800

    Formatted file, Added comments

--- sign-in.js
+++ sign-in.js
@@ -1,62 +1,72 @@
-function renderButton() {
-  gapi.signin2.render('g-signin2', {
-    'scope': 'profile email',
-    'width': 240,
-    'height': 50,
-    'longtitle': true,
-    'theme': 'dark',
-    'onsuccess': onSuccess,
-    'onfailure': onFailure
-  });
+/*global document, window, alert, console, require, google, gapi, moment, $, setTimeout*/
+
+var GrowlNotification;
+
+// Welcome user notification
+function welcome(profile) {
+    "use strict";
+    console.log("Welcome notification loading...");
+    GrowlNotification.notify({
+        title: "Welcome " + profile.getName().split(" ")[0],
+        description: '',
+        image: {
+            visible: true,
+            customImage: ''
+            //profile.getImageUrl()
+        },
+        closeTimeout: 3000,
+        type: 'default',
+        closeWith: ['click', 'button'],
+        animationDuration: 0.5,
+        position: 'top-center'
+    });
+    console.log("Welcome complete!!");
 }
 
-function onFailure(error) {
-  console.log(error);
-}
-
-function onSuccess(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  // console.log("Successful login. Loading user info...")
-  // console.log('ID: ' + profile.getId());
-  // console.log('Name: ' + profile.getName());
-  // console.log('Image URL: ' + profile.getImageUrl());
-  // console.log('Email: ' + profile.getEmail());
-  //pass information to server to insert or update the user record
-  window.location.href = '#';
-  setTimeout(function() {welcome(profile);}, 1000);
-  updateUserData(profile);
-}
-
-function welcome(profile){
-  console.log("Welcome notification loading...");
-  GrowlNotification.notify({
-    title: "Welcome " + profile.getName().split(" ")[0],
-    description: '',
-    image: {
-      visible: true,
-      customImage: ''
-      //profile.getImageUrl()
-    },
-    closeTimeout: 3000,
-    type: 'default',
-    closeWith: ['click', 'button'],
-    animationDuration: .5,
-    position: 'top-center'
-
-});
-console.log("Welcome complete!!");
-// or (new GrowlNotification(options)).show();
-}
-
-function updateUserData(profile){
-  if(profile){
-      $.ajax({
+// Sends user details to save-user
+function updateUserData(profile) {
+    "use strict";
+    if (profile) {
+        $.ajax({
             type: 'POST',
             url: 'save-user',
-            data: {id:profile.getId(), name:profile.getName(), email:profile.getEmail()}
-        }).done(function(data){
+            data: {
+                id: profile.getId(),
+                name: profile.getName(),
+                email: profile.getEmail()
+            }
+        }).done(function (data) {
             console.log(data);
             window.location.href = '#';
-        })
-  } 
+        });
+    }
+}
+
+// Failed google login
+function onFailure(error) {
+    "use strict";
+    console.log(error);
+}
+
+// Successful google login, trigger welcome notification
+function onSuccess(googleUser) {
+    "use strict";
+    var profile = googleUser.getBasicProfile();
+    window.location.href = '#';
+    setTimeout(function () {welcome(profile); }, 1000);
+    updateUserData(profile);
+}
+
+// Log user in
+function renderButton() {
+    "use strict";
+    gapi.signin2.render('g-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
 }
